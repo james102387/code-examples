5db3e1f5a465aa0308d6ff1c4e7da3f0917eb2a8
James Potts <james102387@gmail.com>
2023-08-21 20:20:39 -0700

    Create chat-template.php

---
+++ chat-template.php
@@ -0,0 +1,5 @@
+<div id="simple-chat-container">
+    <div id="chat-messages"></div>
+    <input type="text" id="chat-input" placeholder="Type your message...">
+    <button id="send-button">Send</button>
+</div>
